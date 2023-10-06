a2bf6f29d9e048d5b7a4ab8fbb7a12ddccd5425c
Ayush Singh <ayushsingh2442@gmail.com>
2023-10-06 13:46:05 +0530

    First Submit

--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -3,10 +3,9 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("books")
@@ -20,4 +19,35 @@ public class BookController {
         Book newbook = bookService.createBook(book);
         return new ResponseEntity<>(newbook, HttpStatus.CREATED);
     }
+    @GetMapping("/find-book-by-id")
+    public ResponseEntity findBookById(@RequestParam("id")String id) {
+            Book bk = bookService.findBookById(id);
+            return new ResponseEntity<>(bk,HttpStatus.ACCEPTED);
+    }
+    @DeleteMapping("/delete-book-by-id")
+    public ResponseEntity deleteBookById(@RequestParam String id) {
+        bookService.deleteBookById(id);
+        return new ResponseEntity<>("Book Delete Succesfully",HttpStatus.ACCEPTED);
+    }
+    @GetMapping("/all-book")
+    public ResponseEntity findAllBooks(){
+        List<Book> book = bookService.findAllBooks();
+        return new ResponseEntity<>(book,HttpStatus.OK);
+    }
+    @DeleteMapping("/delete-all-book")
+    public ResponseEntity deleteAllBooks(){
+        bookService.deleteAllBooks();
+        return new ResponseEntity<>("Delete Success",HttpStatus.OK);
+    }
+    @GetMapping("/find-book-by-author")
+    public ResponseEntity findBooksByAuthor(@RequestParam("author")String author) {
+        List<Book> bk = bookService.findBooksByAuthor(author);
+        return new ResponseEntity(bk,HttpStatus.OK);
+    }
+    @GetMapping("/find-book-by-genre")
+    public ResponseEntity findBooksByGenre(@RequestParam String genre){
+        List<Book> bk = bookService.findBooksByGenre(genre);
+        return new ResponseEntity<>(bk,HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -5,37 +5,67 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepository {
-
+    HashMap<Integer,Book> bookDb;
+    HashMap<String,List<Book>>authorDb;
     public BookRepository(){
-        
+        bookDb = new HashMap<>();
+        authorDb = new HashMap<>();
     }
 
     public Book save(Book book){
-        return null;
+        Book bk = new Book();
+        bk.setId(book.getId());
+        bk.setName(book.getName());
+        bk.setGenre(book.getGenre());
+        bk.setAuthor(book.getAuthor());
+        bookDb.put(bk.getId(), bk);
+        List<Book>bookListByAuthor = new ArrayList<>();
+        bookListByAuthor.add(bk);
+        authorDb.put(book.getAuthor(),bookListByAuthor);
+        return bk;
     }
 
     public Book findBookById(int id){
-
-        return null;
+        Book bk = bookDb.get(id);
+        return bk;
     }
 
     public List<Book> findAll(){
-        return null;
+
+        List<Book>lt = new ArrayList<>();
+        for(int key : bookDb.keySet()){
+            lt.add(bookDb.get(key));
+        }
+        return lt;
     }
 
     public void deleteBookById(int id){
-        return;
+        Book bk = bookDb.get(id);
+        authorDb.remove(bk.getAuthor());
+        bookDb.remove(id);
     }
 
     public void deleteAll(){
-        return;
+        for(int key : bookDb.keySet()){
+            authorDb.remove(bookDb.get(key).getAuthor());
+            bookDb.remove(key);
+        }
     }
 
     public List<Book> findBooksByAuthor(String author){
-        return null;
+        List<Book>bookList = new ArrayList<>();
+        bookList = authorDb.get(author);
+        return bookList;
+
     }
 
     public List<Book> findBooksByGenre(String genre){
-        return null;
+        List<Book> lb = new ArrayList<>();
+        for(int key : bookDb.keySet()) {
+            if(bookDb.get(key).getGenre().equals(genre)){
+                lb.add(bookDb.get(key));
+            }
+        }
+        return lb;
     }
 }
